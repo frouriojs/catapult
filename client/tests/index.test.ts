e9ab6379f036e096a9e74f18e1fbd94a51de3377
solufa <solufa2020@gmail.com>
2024-08-16 23:54:19 +0900

    chore: add OpenAPI diff test

--- client/tests/index.test.ts
+++ client/tests/index.test.ts
@@ -1,5 +1,12 @@
+import { toOpenAPI } from 'aspida2openapi';
+import { readFileSync } from 'fs';
+import { join } from 'path';
+import { staticPath } from 'utils/$path';
 import { expect, test } from 'vitest';
 
-test('default', () => {
-  expect(true).toBeTruthy();
+test('check OpenAPI diff', () => {
+  const openapi = join('./public', staticPath.docs.openapi_json);
+  const generated = toOpenAPI({ input: 'api', template: openapi });
+
+  expect(generated).toBe(readFileSync(openapi, 'utf8'));
 });
