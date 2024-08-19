3500f75e7936b839baa039687e6ddfe8567e5c41
solufa <solufa2020@gmail.com>
2024-08-19 14:48:26 +0900

    chore: update aspida2openapi to v0.5.0

--- client/pages/docs.page.tsx
+++ client/pages/docs.page.tsx
@@ -2,6 +2,6 @@ import SwaggerUI from 'swagger-ui-react';
 import 'swagger-ui-react/swagger-ui.css';
 import { staticPath } from 'utils/$path';
 
-const Docs = () => <SwaggerUI url={staticPath.docs.openapi_json} docExpansion="none" />;
+const Docs = () => <SwaggerUI url={staticPath.docs.openapi_json} docExpansion="list" />;
 
 export default Docs;
