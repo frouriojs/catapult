92e2cba16216d04aae213bf130a775aff370539b
solufa <solufa2020@gmail.com>
2024-08-21 01:02:06 +0900

    chore: remove openapi test

--- client/tests/index.test.ts
+++ client/tests/index.test.ts
@@ -1,12 +1,5 @@
-import { toOpenAPI } from 'aspida2openapi';
-import { readFileSync } from 'fs';
-import { join } from 'path';
-import { staticPath } from 'utils/$path';
 import { expect, test } from 'vitest';
 
-test('check OpenAPI diff', () => {
-  const openapi = join('./public', staticPath.docs.openapi_json);
-  const generated = toOpenAPI({ input: 'api', template: openapi });
-
-  expect(generated).toBe(readFileSync(openapi, 'utf8'));
+test('default', () => {
+  expect(true).toBeTruthy();
 });
