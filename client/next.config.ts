f5935f2232de3ea6eb45bcd72f33d083dbdf1493
solufa <solufa2020@gmail.com>
2024-12-14 19:53:53 +0900

    chore: rename next.config.js to ts

---
+++ client/next.config.ts
@@ -0,0 +1,23 @@
+import type { NextConfig } from 'next';
+import { NEXT_PUBLIC_API_BASE_PATH, NEXT_PUBLIC_SERVER_PORT } from 'utils/envValues';
+
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  pageExtensions: ['page.tsx'],
+  trailingSlash: true,
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
+  webpack: (config) => {
+    config.resolve.symlinks = false;
+
+    return config;
+  },
+  rewrites: async () => [
+    {
+      source: `${NEXT_PUBLIC_API_BASE_PATH}/:path*`,
+      destination: `http://localhost:${NEXT_PUBLIC_SERVER_PORT}${NEXT_PUBLIC_API_BASE_PATH}/:path*`,
+    },
+  ],
+};
+
+export default nextConfig;
